49d5679be7eea842e8ab75d500df45a4724e9b23
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2022-01-07 13:25:49 +0100

    start 3.2.2

---
+++ src/listings/3.2/GenerateResult.ts
@@ -0,0 +1,8 @@
+export interface GenerateResult {
+  board: string,
+  pages: Map<number, string>,
+  numberOfPages: number,
+  diagrams: Map<number, string>,
+  numberOfDiagrams: number,
+  classDiagram: string,
+}
