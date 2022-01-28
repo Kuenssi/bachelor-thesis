a3be99a5c8cf296c56ee5207178a0a4bf7a3779c
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2022-01-28 16:09:21 +0100

    added the last corrections from The Täsch for chapter 3

--- src/listings/3.3/Endpoints.java
+++ src/listings/3.3/Endpoints.java
@@ -1,14 +1,18 @@
-@PostMapping(path = "/generate", consumes = MediaType.ALL_VALUE)
-@ResponseBody
-public String generate(@RequestBody String yamlData) {
-    return fulibWorkflowsService.generate(yamlData);
-}
+@Controller
+@CrossOrigin()
+public class FulibWorkflowsController {
+    @PostMapping(path = "/generate", consumes = MediaType.ALL_VALUE)
+    @ResponseBody
+    public String generate(@RequestBody String yamlData) {
+        return fulibWorkflowsService.generate(yamlData);
+    }
 
-@PostMapping(path = "/download",
-             consumes = MediaType.ALL_VALUE,
-             produces = "application/zip")
-@ResponseBody
-public byte[] download(@RequestBody String yamlData,
-                       @RequestParam Map<String, String> queryParams) {
-    return fulibWorkflowsService.createZip(yamlData, queryParams);
+    @PostMapping(path = "/download",
+            consumes = MediaType.ALL_VALUE,
+            produces = "application/zip")
+    @ResponseBody
+    public byte[] download(@RequestBody String yamlData,
+                           @RequestParam Map<String, String> queryParams) {
+        return fulibWorkflowsService.createZip(yamlData, queryParams);
+    }
 }
